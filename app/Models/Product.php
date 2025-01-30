3836e7ecb8ed6fda0ca891c09746de127e65ee07
Yeray Navarro <yeraynp16@gmail.com>
2025-01-30 16:46:35 +0000

    Categorias y modificaciones a product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,9 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'description', 'price', 'stock'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
